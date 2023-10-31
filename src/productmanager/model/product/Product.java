ef9f65223135f790eac9c7eec92e76cd555f3f81
alezxandro <gheojan.alessandro@outlook.com>
2023-10-31 14:40:30 +0100

    Update Product.java

--- src/productmanager/model/product/Product.java
+++ src/productmanager/model/product/Product.java
@@ -1,5 +1,36 @@
 package productmanager.model.product;
 
 public class Product {
+    private int id;
+    private String title;
+    private String description;
+
+    
+
+    public Product(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
     
 }
