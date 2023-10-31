c65ea2f36b329c8f60764ce6036ce653f1b64788
alezxandro <gheojan.alessandro@outlook.com>
2023-10-31 16:19:56 +0100

    Update User.java

--- src/productmanager/model/user/User.java
+++ src/productmanager/model/user/User.java
@@ -1,5 +1,50 @@
 package productmanager.model.user;
 
 public class User {
+    private static int idCounter = 1;
+    private int id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.id = idCounter++;
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean checkPassword (String inputPassword) {
+        return inputPassword.equals(password);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
+
+    
+
+    
+
+
 }
